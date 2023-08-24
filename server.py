593245f60b11fc1b27029fca2c04025b0fab6b58
mouctarbarry <mouctar.barry@sogeti.com>
2023-08-24 21:26:15 +0200

    maj server port

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 import http.server
 import socketserver
+import ssl
+import os
 
-PORT = 8000
+PORT = int(os.environ.get("PORT", 8000))  # Utilisez la variable d'environnement PORT ou 8000 par défaut
 
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
     pass
 
 
-with socketserver.TCPServer(("", PORT), MyRequestHandler) as httpd:
-    print(f"Serving at port {PORT}")
-    httpd.serve_forever()
+httpd = socketserver.TCPServer(("", PORT), MyRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile="server.pem", server_side=True)
+
+print(f"Serving at port {PORT}")
+httpd.serve_forever()
